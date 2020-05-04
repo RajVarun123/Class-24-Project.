3b6862b690fd59e5830c95aa70b290047123ae08
RajVarun123 <63353838+RajVarun123@users.noreply.github.com>
2020-05-04 03:11:53 -0700

    Add files via upload

---
+++ js/BouncyBall.js
@@ -0,0 +1,25 @@
+class BouncyBall {
+    constructor(x,y,radius) {
+      var options = {
+          frictionAir: 0,
+          friction: 0,
+          frictionStatic: 1,
+          inertia: Infinity,
+          restitution: 1.0
+      }
+  
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius = radius;
+      
+      World.add(world, this.body);
+    }
+  
+    display() {
+      var pos = this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      fill("red");
+      ellipse(0, 0, this.radius);
+      pop();
+    }
+  }
